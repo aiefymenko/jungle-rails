213da1141ef15a1a0d30f8a11718444f321aff63
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-07-19 13:58:11 -0400

    Test user signup and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: {in: 6..20}, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    email.strip!
+    email.downcase!
+    @user = User.find_by_email(email)
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 
 end
